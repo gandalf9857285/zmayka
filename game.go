2096a6b805c070dc203060223e92c6bb4dd45084
gandalf9857285 <sofia.burh@gmail.com>
2023-09-07 00:09:16 +0300

    0:09

---
+++ game.go
@@ -0,0 +1,29 @@
+package sneke
+
+type Point struct {
+	X, Y float64
+}
+
+const (
+	Top    = iota
+	Right  = iota
+	Bottom = iota
+	Left   = iota
+)
+
+type Dir int
+
+func (d Dir) Exec(p Point) Point {
+	switch d {
+	case Top:
+		return Point{p.X, p.Y + 1}
+	case Right:
+		return Point{p.X + 1, p.Y}
+	case Bottom:
+		return Point{p.X, p.Y - 1}
+	case Left:
+		return Point{p.X - 1, p.Y}
+	default:
+		return p
+	}
+}
